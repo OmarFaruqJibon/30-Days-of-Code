ffaee81b4c683038eb7fbe20c2c2ae9f9448eaf2
MD Omar Faruq <omarfarukjibon418@gmail.com>
2021-02-06 11:25:42 +0600

    Day 6 code updated

--- Day_6_Lets_Review/Solution.java
+++ Day_6_Lets_Review/Solution.java
@@ -1,9 +1,36 @@
 package Day_6_Lets_Review;
 
+import java.util.Scanner;
+
 public class Solution {
 
 	public static void main(String[] args) {
-
+		Scanner myScanner = new Scanner(System.in);
+		
+		int t = myScanner.nextInt();
+		myScanner.nextLine();
+		
+		
+		
+		for (int i = 0; i < t; i++) {
+			String s;
+			
+			String even = "";
+			String odd = "";
+			s = myScanner.nextLine();
+			
+			for (int j = 0; j < s.length(); j++) {
+				
+				if(j %2== 0) {
+					even+=s.charAt(j);
+				}
+				else {
+					odd+=s.charAt(j);
+				}
+				
+			}
+			System.out.println(even+" "+odd);
+		}
+		
 	}
-
 }
